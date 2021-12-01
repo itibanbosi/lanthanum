23d5f22018ff16ecd7d247f32574f7e72902de63
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-12-01 11:57:49 +0900

    更新 custom.ts

--- custom.ts
+++ custom.ts
@@ -72,9 +72,7 @@ namespace natumi_blocks {
 
     //% color="#858585" weight=54 blockId=fullcolor_power block="ＬＥＤ|%color|色 明るさ|%power|" group="2_にじ色ＬＥＤ"
     export function fullcolor_power(color: LED_color, power: number) {
-        pins.digitalWritePin(DigitalPin.P1, 0);
-        pins.digitalWritePin(DigitalPin.P15, 0);
-        pins.digitalWritePin(DigitalPin.P16, 0);
+
         switch (color) {
             case LED_color.Red:
                 pins.analogWritePin(AnalogPin.P1, 0)
